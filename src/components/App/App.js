f654c3f6ce2e34d1442bb50db832040b5961cbdd
Gus <guusflorez@gmail.com>
2018-07-10 12:29:59 +0200

    displaying hardcoded data succesfully

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import Playlist from '../Playlist/Playlist';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
-import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      SearchResults: [
+        { name: 'Bon', artist: 'Jovi', album: 'My Life', id: '1' },
+        { name: 'Erros', artist: 'Rama', album: 'LOL', id: '2' },
+        { name: 'Super', artist: 'Tranmp', album: 'Sandman', id: '3' }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +24,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.SearchResults} />
             <Playlist />
           </div>
         </div>
